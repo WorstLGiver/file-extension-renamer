0f7dbde9555653f922b4e3254630165149287f2a
WorstLGiver <83733010+WorstLGiver@users.noreply.github.com>
2026-06-13 20:15:56 -0400

    Add files via upload

---
+++ rename_ext.py
@@ -0,0 +1,15 @@
+import os
+
+folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "")
+print("Put the full file extension with the dot or leave empty.")
+target_ext = input("What file extension do you want to replace? ")
+replace_ext = input("What do you want to replace it with? ")
+
+if target_ext == "":
+    for files in os.listdir(folder):
+        if "." not in files:
+            os.rename(folder + files, folder + files + replace_ext)
+else:
+    for files in os.listdir(folder):
+        if files.endswith(target_ext):
+            os.rename(folder + files, folder + files[:-(len(target_ext))] + replace_ext)
